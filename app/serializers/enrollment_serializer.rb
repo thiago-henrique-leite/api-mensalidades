7c789e317b0c070a1703ff83ccd3dee0e5f58c34
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-08-14 15:53:55 -0300

    feat: add student name in enrollment serializer

--- app/serializers/enrollment_serializer.rb
+++ app/serializers/enrollment_serializer.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class EnrollmentSerializer < ActiveModel::Serializer
-  attributes :id, :student_id, :amount, :installments, :due_day, :bills
+  attributes :id, :student_id, :student_name, :amount, :installments, :due_day, :bills
 
   def bills
     object.bills.map { |bill| BillSerializer.new(bill).as_json }
+  end
+
+  def student_name
+    object.student.name
   end
 end
