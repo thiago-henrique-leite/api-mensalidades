e859f38fc0966a91a8bf5186f31a95b36714cd55
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2023-01-08 13:36:12 -0300

    refactor: improve code

---
+++ app/controllers/api/enrollments_controller.rb
@@ -0,0 +1,45 @@
+class Api::EnrollmentsController < ApplicationController
+  http_basic_authenticate_with name: Settings.auth.username, password: Settings.auth.password
+
+  def index
+    render json: enrollments
+  end
+
+  def show
+    render json: enrollment
+  end
+
+  def create
+    enrollment = Enrollment.create(enrollment_params)
+
+    return render json: enrollment, status: :created if enrollment.save
+
+    render json: { error: enrollment.errors }, status: :bad_request
+  end
+
+  def update
+    enrollment.update!(enrollment_params)
+
+    render json: enrollment
+  end
+
+  def destroy
+    enrollment.destroy!
+
+    render json: {}, status: :no_content
+  end
+
+  private
+
+  def enrollment
+    @enrollment ||= Enrollment.find(params[:id])
+  end
+
+  def enrollments
+    @enrollments ||= Enrollment.page(page).per(count)
+  end
+
+  def enrollment_params
+    params.permit(:amount, :due_day, :installments, :student_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace 'api' do
-    namespace 'v1' do
-      resources :students, :enrollments, :bills
-    end
+  namespace :api do
+    resources :students
+    resources :enrollments
+    resources :bills, except: [:create]
   end
 end
