052147ff009a47eb50e37794e3c0e0b72edfdb97
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2023-02-02 16:36:09 -0300

    fix: update enrollment installments seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ ActiveRecord::Base.transaction do
     Enrollment.create!(
       student: Student.last,
       amount: Faker::Number.decimal(l_digits: 4, r_digits: 2),
-      installments: Faker::Number.between(from: 1, to: 10),
+      installments: Faker::Number.between(from: 2, to: 10),
       due_day: Faker::Number.between(from: 1, to: 31)
     )
   end
